8f265ae06b71609d4310d9726483a79afe6768d5
coulou800 <coulou800@gmail.com>
2024-08-11 06:36:43 +0000

    add animation and player model

--- src/player/systems.rs
+++ src/player/systems.rs
@@ -9,30 +9,34 @@ pub fn player_system(mut query: Query<&mut Player>) {
     }
 }
 
-pub fn spawn_player(mut commands: Commands, ass: Res<AssetServer>,    mut graphs: ResMut<Assets<AnimationGraph>>,) {
-// Build the animation graph
-let mut graph = AnimationGraph::new();
-let animations = graph
-    .add_clips(
-        [
-            // GltfAssetLabel::Animation(2).from_asset("player1.glb"),
-            // GltfAssetLabel::Animation(1).from_asset("player1.glb"),
-            GltfAssetLabel::Animation(0).from_asset("player1.glb"),
-        ]
-        .into_iter()
-        .map(|path| ass.load(path)),
-        1.0,
-        graph.root,
-    )
-    .collect();
+pub fn spawn_player(
+    mut commands: Commands,
+    ass: Res<AssetServer>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    // Build the animation graph
+    let mut graph = AnimationGraph::new();
+    let animations = graph
+        .add_clips(
+            [
+                GltfAssetLabel::Animation(2).from_asset("player1.glb"),
+                GltfAssetLabel::Animation(1).from_asset("player1.glb"),
+                GltfAssetLabel::Animation(0).from_asset("player1.glb"),
+            ]
+            .into_iter()
+            .map(|path| ass.load(path)),
+            1.0,
+            graph.root,
+        )
+        .collect();
 
-// Insert a resource with the current scene information
-let graph = graphs.add(graph);
-commands.insert_resource(Animations {
-    animations,
-    graph: graph.clone(),
-});
-let model = ass.load("player1.glb#Scene0");
+    // Insert a resource with the current scene information
+    let graph = graphs.add(graph);
+    commands.insert_resource(Animations {
+        animations,
+        graph: graph.clone(),
+    });
+    let model = ass.load("player1.glb#Scene0");
     // Spawn player entity
     commands.spawn(
         (SceneBundle {
@@ -56,7 +60,7 @@ pub fn player_animation(
         // the animations and will get confused if the animations are started
         // directly via the `AnimationPlayer`.
         transitions
-            .play(&mut player, animations.animations[0], Duration::ZERO)
+            .play(&mut player, animations.animations[1], Duration::ZERO)
             .repeat();
 
         commands
